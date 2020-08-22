b495419b486a868978a78cf2032af170cc9957ec
Greice Felipe <greice.n.felipe@gmail.com>
2020-08-22 19:26:24 -0300

    Corrected rubocop offense.

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# app/controllers/home_page_controller.rb
 class HomePageController < ApplicationController
-  def index
-  end
+  # GET index
+  def index; end
 end
